9a37c7dbaa4cc51fffcb98377d3103650c37a396
jamsheerc22 <39427091+jamsheerc22@users.noreply.github.com>
2018-05-23 08:19:40 +0530

    Update script.py

--- script.py
+++ script.py
@@ -1,1 +1,9 @@
 
+def display_board(board):
+    """This is a square board with 9 boxs"""
+    print(" {} | {} | {} ".format(*board[0:3]))
+    print("---|---|---")
+    print(" {} | {} | {} ".format(*board[3:6]))
+    print("---|---|---")
+    print(" {} | {} | {} ".format(*board[6:9]))
+    
