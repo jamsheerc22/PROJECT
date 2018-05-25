e26593368e6e03ff7da8c9077aba6df8575e478e
jamsheerc22 <39427091+jamsheerc22@users.noreply.github.com>
2018-05-25 12:04:08 +0530

    Update script.py

--- script.py
+++ script.py
@@ -27,4 +27,12 @@ player1,player2 = player_input()
 def place_marker(board,marker,position):
     board[position]=marker
     
+#below is the function to check who has won
+
+def win_check(board,mark):
+    '''if 3 boxs in a row are equal or 3 boxs in a column are equal or also diagnolly equal, then that player is the winner'''
+    return ((board[1]==board[2]==board[3]==mark) or (board[4]==board[5]==board[6]==mark) or (board[7]==board[8]==board[9]==mark) or
+    (board[1]==board[4]==board[7]==mark) or (board[2]==board[5]==board[8]==mark) or (board[3]==board[6]==board[9]==mark) or
+    (board[1]==board[5]==board[9]==mark) or (board[7]==board[5]==board[3]==mark) )
+    
     
